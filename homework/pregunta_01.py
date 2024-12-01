d8fef39714a68018e2695f0cd7a565cc98ae8235
paristizabalc <paristizabalc@unal.edu.co>
2024-12-01 17:53:09 -0500

    Lab 03

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,6 +4,9 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
+import re
+
+import pandas as pd
 
 def pregunta_01():
     """
@@ -18,3 +21,63 @@ def pregunta_01():
 
 
     """
+    def extract_records(data):
+        """Extraer registos empleando regex"""
+        pattern = re.compile(
+            r"([0-9]+)(?:\s+)([0-9]+)(?:\s+)([0-9|\,]+)(?:[\s|\%]+)([a-z|\s|\,|\-|\/|\n|(|)]+)(?:\.*\n*)"
+        )
+
+        return pattern.findall(data)
+
+    def process_key_words(data):
+        """Procesar palabras clave"""
+
+        def concat_key_words(key_words):
+            """Concatenar palabras clave"""
+            key_words = key_words.replace("\n", " ").split(",")
+            return ", ".join([" ".join(word.strip().split()) for word in key_words])
+
+        data = [
+            (
+                int(record[0]),
+                float(record[1]),
+                float(record[2].replace(",", ".")),
+                concat_key_words(record[3]),
+            )
+            for record in data
+        ]
+
+        return data
+
+    def create_dataframe(data):
+        """Crear dataframe"""
+        columns = [
+            "cluster",
+            "cantidad_de_palabras_clave",
+            "porcentaje_de_palabras_clave",
+            "principales_palabras_clave",
+        ]
+        df = pd.DataFrame(data, columns=columns)
+
+        return df
+
+    def read_text_file(file_path):
+        """Leer archivo de texto"""
+        with open(file_path, "r") as file:  # type: ignore
+            data = file.read()
+
+        return data
+
+    def run_job():
+        data = read_text_file("files/input/clusters_report.txt")
+        data = extract_records(data)
+        data = process_key_words(data)
+        data = create_dataframe(data)
+
+        return data
+
+    return run_job()
+
+
+if __name__ == "__main__":
+    print(pregunta_01())
